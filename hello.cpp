166e1838a287e4dfcdcb30c25357e97e16e0b612
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-01-24 00:35:51 +0530

    new change added

--- hello.cpp
+++ hello.cpp
@@ -4,8 +4,7 @@ using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    cout << "Hey Anjali! Reetik really likes you a lot." << endl;
-    cout << "Ignores me when you call" << endl;
+    cout << "git" << endl;
     return 0;
 }
 
