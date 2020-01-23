bf4159ad2a596a37fe9fa3962bb52a373a707dc2
Reetik <reetik.raj.chy17@itbhu.ac.in>
2020-01-24 00:00:46 +0530

    initial commit added

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cout << "Hey Anjali! Reetik really likes you a lot." << endl;
+    cout << "Ignores me when you call" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
